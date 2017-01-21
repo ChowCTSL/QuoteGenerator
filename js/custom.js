07e2d392086fc816342c5d62f29eb0769124d15d
chowctsl <chow.ctsl@gmail.com>
2017-01-21 14:19:11 -0700

    Edited JS and transition for the bgcolor

--- js/custom.js
+++ js/custom.js
@@ -20,15 +20,15 @@ function getQuote() {
 function changeBgColor() {
 	//array of possible bg colors
 	var colors = ["rgb(0, 0, 0)", "rgb(26, 188, 156)", "rgb(243, 156, 18)", "rgb(52, 152, 219)", "rgb(231, 76, 60)", 
-	"rgb(52, 73, 94)", "rgb(155, 89, 182)", "rgb(46, 204, 113)", "rgb(127, 140, 141)", "rgb(246, 36, 89)", "rgb(82, 179, 217)"];
+	"rgb(52, 73, 94)", "rgb(155, 89, 182)", "rgb(46, 204, 113)", "rgb(127, 140, 141)", "rgb(246, 36, 89)"];
 
 	var index = colors.indexOf($("html,body").css("background-color"));
 
 	//prevent duplicate colors on fade in
-	if(index > -1) {
+	if(index !== -1) {
 		colors.splice(index, 1);
 	}
-	
+
 	//randomly choose a bg color from the array
 	var randomNumber = Math.floor(Math.random() * (colors.length - 1 + 1) + 1);
 	$("html, body").css("background-color", colors[randomNumber]);
